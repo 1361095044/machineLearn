048bcbb827577645102017b585650fd29a0a064f
zc <1361095044@qq.com>
2022-10-11 10:37:44 +0800

    机器学习课程

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # This is a sample Python script.
 import matplotlib.pyplot as plt
 
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
